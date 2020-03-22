942d1f811f4d2bdea768a27dde1315f1e1e4be0b
Somnus <nimugates@gmail.com>
2020-03-22 19:52:59 +0100

    Initialized project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+	/* include_once(""); */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Index</title>
+	<link href="assets/styles.css" media="all" rel="stylesheet" type="text/css"/>
+	
+	
+</head>
+	<body>
+			<?php
+				include("public/main/controller.php");
+			?>
+	</body>
+</html>
