826a68ef0b5b305d2ec43d04a05a1d41f2f7e941
Somnus <nimugates@gmail.com>
2020-03-22 21:09:34 +0100

    Initializing files

--- index.php
+++ index.php
@@ -8,9 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Index</title>
-	<link href="assets/styles.css" media="all" rel="stylesheet" type="text/css"/>
-	
-	
+	<link href="assets/styles.css" media="all" rel="stylesheet" type="text/css"/>	
 </head>
 	<body>
 			<?php
---
+++ public/main/controller.php
@@ -0,0 +1,36 @@
+        <header>
+			<div>
+				<div class="menu">
+					<?php
+							include("public/header/downloadModule.php"); 
+					?>
+				</div>
+			    <div id="logo">
+					<?php
+							include("public/header/logoModule.php"); 
+					?>
+				</div>
+				<div id="login">
+						<?php
+								include("public/header/private/loginModule.php");
+						?>
+				</div>
+			</div>
+		</header>
+		<nav>
+            <div class="menu">
+                <?php
+                        include("public/nav/navbarModule.php");
+                ?>
+            </div>
+        </nav>
+        <main>
+			<?php
+					include("public/main/mainModule.php");
+            ?>
+        </main>
+		<footer>
+			<?php
+				include("public/footer/footerModule.php"); 
+			?>
+		</footer>
