fd3949067441f3d896a6c231734c0ac8724dfd18
Somnus <nimugates@gmail.com>
2020-03-23 01:13:32 +0100

    db login added

---
+++ public/main/private/db_con.php
@@ -0,0 +1,17 @@
+<?php
+    // Store db connection variables
+    $hostName = "localhost";
+    $usrName = "root";
+    $dbPssword = "";
+    $databaseName = "phplogin";
+
+    // We make a connection to the db with those variables
+    $link = mysqli_connect($hostName, $usrName, $dbPssword, $databaseName);
+
+    // If connection gave an error / was false
+    if (!$link)
+    {
+        // We store the error message to be printed on main module
+        $_SESSION['error'] = 'Connect Error: ' . mysqli_connect_error();
+    }
+?>
