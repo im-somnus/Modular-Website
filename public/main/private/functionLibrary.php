879a73665cebe89b55caad2635ae5a10d31ec95a
Somnus <nimugates@gmail.com>
2020-03-23 01:19:37 +0100

    added functionLibrary, included basic functions

---
+++ public/main/private/functionLibrary.php
@@ -0,0 +1,115 @@
+<?php
+
+/* ####################################### TESTING TOOLS ####################################### */
+
+// function to dump all variables (test)
+function dump($v)
+{
+    echo '<pre>';
+        var_dump($v);
+    echo '<pre>';
+}
+
+/* ####################################### QUERY/SQL FUNCTIONS ####################################### */
+
+// Function to shorten the queries, sends 0 if the query failed, 1 if it worked.
+function verifyQuery($query)
+{
+    require_once("db_con.php");  
+    
+    if ($result = mysqli_query($link, $query)) 
+    { 
+        mysqli_close($link);
+        // Returns true if success Query
+        return 1;
+    }
+    else
+    {
+        mysqli_close($link); 
+        // Returns false if failed Query
+        return 0;
+    }
+}
+
+/* ####################################### USERS FUNCTIONS ####################################### */
+
+// Set to 1 the user status (online)
+function setOnlineStatus($user)
+{
+    require_once("db_con.php");
+
+    $sql = "UPDATE accounts SET userStatus='1' where username='$user';";
+    $result = mysqli_query($link, $sql);
+
+    mysqli_close($link);
+}
+
+// Function to update lastActivity timestamp in the database using $_SESSION when loading pages.
+function keepOnlineStatus($user)
+{
+    require_once("db_con.php");
+    // Set default timezone
+    date_default_timezone_set('Europe/Madrid');
+    // Store custom date format in a variable
+    $date = date('Y-m-d H:i:s');
+
+    // Update user user last activity with current time
+    $sql = "UPDATE accounts SET lastActivity='$date' where username='$user';";
+    mysqli_query($link, $sql);
+
+    // Function that sets online status to "online"
+    setOnlineStatus($user);
+}
+
+// Set to 0 the user status (offline).
+function setOfflineStatus($user)
+{
+    require_once("db_con.php");
+    // update status to 0 (offline)
+    $sql = "UPDATE accounts SET userStatus='0' where username='$user';";
+    mysqli_query($link, $sql);
+
+    mysqli_close($link);
+}
+
+// Function to logout the users, destroy all session variables etc.
+function logout($user)
+{
+    // We set the user status from online (1) to offline (0)
+    $user = $_SESSION['login']['user'];
+    setOfflineStatus($user);
+
+    // Unset the session variables.
+    unset($_SESSION['login']);
+    unset($_SESSION["rank"]);
+    session_destroy();
+    
+    // Redirect to the login page:
+    header('Location: ../index.php');
+    exit();
+}
+
+
+/*  ####################################### POST FUNCTIONS ####################################### */
+
+
+
+/*  ####################################### THREAD FUNCTIONS ####################################### */
+
+
+
+
+/*  ####################################### CATEGORIES FUNCTIONS ####################################### */
+
+
+
+
+
+z
+
+
+
+
+
+
+?>
