81dd02cc6a0103900a667b04509eea1451b17c29
Somnus <nimugates@gmail.com>
2020-03-23 01:11:58 +0100

    fixed includes to only include them once

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	session_start();
-	/* include_once(""); */
+	include_once("public/main/private/functionLibrary.php");
+	include_once("public/main/private/db_con.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
