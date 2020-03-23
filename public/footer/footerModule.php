09baffa8ba874bf3f1786ecfd3a6ff0a00031336
Somnus <nimugates@gmail.com>
2020-03-23 01:12:46 +0100

    added footer module

---
+++ public/footer/footerModule.php
@@ -0,0 +1,37 @@
+<!-- This file is the module of the footer  -->
+<div class="row">
+  <div class="column">
+      <ol>
+        <li><b><a href='index.php'>Home</a></b></li>
+        <li><a href="index.php?profile">Profile</a></li>
+      </ol>
+  </div>
+  <div class="column">
+      <ol>
+        <li><b><a href='index.php?overview'>Game</a></b></li>
+        <li><a href="index.php?videos">Videos</a></li>
+        <li><a href="index.php?">Download</a></li>
+      </ol>
+  </div>
+  <div class="column">
+      <ol>
+        <li><b><a href="index.php?forum">Forum</a></b></li>
+        <li><a href="index.php?thread">Index</a></li>
+        <li> <a href="index.php?code">Code of Conduct</a></li>
+      </ol>
+  </div>
+  <div class="column">
+      <ol>
+        <li><b><a href="index.php?news">News</a></b></li>
+        <li><a href="index.php?updates">Updates</a></li>
+        <li><a href="https://gitlab.com/Somnus/pintegrado/" target="_blank">Gitlab Repo</a></li>
+      </ol>
+  </div>
+  <div class="column">
+      <ol>
+        <li><a href="index.php?shop">Online store</a></b></li>
+        <li><a href="index.php?buy">Buy points!</a></li>
+        <li><a href="index.php?points">Point system</a></li>
+      </ol>
+  </div>
+</div>
