3f67a497ab5b1ba0ccde4063de63282ed0c4b89e
Somnus <nimugates@gmail.com>
2020-03-29 23:17:04 +0200

    index properly fixe

--- index.php
+++ index.php
@@ -5,16 +5,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 	<title>Index</title>	
+	
 	<!-- BOOTSTRAP -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	<link href="assets/styles.css" media="all" rel="stylesheet" type="text/css"/>	
+
 </head>
 	<body>
+	<div class="container">
 			<?php
 				include("public/main/controller.php");
 			?>
+	</div>
 	</body>
 </html>
