d6b69b89ee1f6233be6b3ae6f0337a8072fec824
Somnus <nimugates@gmail.com>
2020-03-30 09:59:52 +0200

    added colors to the messages

--- public/main/private/alertMessageModule.php
+++ public/main/private/alertMessageModule.php
@@ -1,13 +1,13 @@
 <?php 
 if ( isset($_SESSION['error']) )
 {
-    echo $_SESSION['error']. "<br>";
-    unset($_SESSION['error']);  
+    echo "<p style='color: Red';>" . $_SESSION['error']. "<br>";
+    unset($_SESSION['error']); 
 }
 
 if (isset($_SESSION['success']))
 {
-    echo $_SESSION['success'] . "<br>";
+    echo "<p style='color: Green';>" . $_SESSION['success'] . "<br>";
     unset($_SESSION['success']);
 }
 ?>
