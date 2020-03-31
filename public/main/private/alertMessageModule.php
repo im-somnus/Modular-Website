19b8a70dd28d656ced67941d283c5e87f43da321
Somnus <nimugates@gmail.com>
2020-03-31 09:57:38 +0200

    Fixed minor bugs with alert module

--- public/main/private/alertMessageModule.php
+++ public/main/private/alertMessageModule.php
@@ -1,13 +1,25 @@
 <?php 
-if ( isset($_SESSION['error']) )
+
+// If any alert needs to be displayed
+if (isset($_SESSION['error']) || isset($_SESSION['success']))
 {
-    echo "<p style='color: Red';>" . $_SESSION['error']. "<br>";
-    unset($_SESSION['error']); 
+    // Create the div to show it
+    echo '<div id="alerts">';
+    
+    // Outputs the errors/successes and unset them (show it only once)
+    if ( isset($_SESSION['error']) )
+    {
+        echo "<p style='color: Red';>" . $_SESSION['error']. "<br>";
+        unset($_SESSION['error']); 
+    }
+
+    if (isset($_SESSION['success']))
+    {
+        echo "<p style='color: Green';>" . $_SESSION['success'] . "<br>";
+        unset($_SESSION['success']);
+    }
+
+    echo "</div>";
 }
 
-if (isset($_SESSION['success']))
-{
-    echo "<p style='color: Green';>" . $_SESSION['success'] . "<br>";
-    unset($_SESSION['success']);
-}
 ?>
