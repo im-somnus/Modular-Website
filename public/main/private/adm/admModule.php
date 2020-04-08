66378e45c6d2e94749bcf6f701d0b1deed9ae4c8
Somnus <nimugates@gmail.com>
2020-04-08 15:56:08 +0200

    added css to the entire admin/mod panel

--- public/main/private/adm/admModule.php
+++ public/main/private/adm/admModule.php
@@ -1,12 +1,32 @@
 <?php
-session_start();
-require("admFunctionLibrary.php");
-require("../functionLibrary.php");
-admRankCheck(1);
+    session_start();
+    require("admFunctionLibrary.php");
+    require("../functionLibrary.php");
+    admRankCheck(1);
+    
+	if (isset($_SESSION['login']))
+	{
+		keepOnlineStatus($_SESSION['login']['user']);
+	}
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Index</title>	
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link href="admstyles.css" media="all" rel="stylesheet" type="text/css"/>	
+</head>
+	<body>
+		<div class="container">
+<div id="index"> 
 <a href="../../../../index.php"><input type="button" value="Index"/></a> 
 
+</div>
 <?php
 
 if (empty( $_POST))
@@ -73,6 +93,8 @@ else
 
 
 ?>
-
-
-
+			
+    
+		</div>
+	</body>
+</html>
--- public/main/private/adm/removePosts.php
+++ public/main/private/adm/removePosts.php
@@ -1,13 +1,38 @@
-
 <?php
-session_start();
-require("admFunctionLibrary.php");
-require("../functionLibrary.php");
+    session_start();
+    require("admFunctionLibrary.php");
+    require("../functionLibrary.php");
+    admRankCheck(1);
+    
+	if (isset($_SESSION['login']))
+	{
+		keepOnlineStatus($_SESSION['login']['user']);
+	}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Index</title>	
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link href="admstyles.css" media="all" rel="stylesheet" type="text/css"/>	
+</head>
+	<body>
+		<div class="container">
             <div class="windowAlert">
 				<?php
 						include("../alertMessageModule.php");
 				?>
-            </div>
-<?php
-removePosts();
+			</div>
+			<div class="windowMain">
+				<?php
+				 	removePosts();
+				?>
+			</div>
+		</div>
+	</body>
+</html>
