716ce978d02720997dabd31cb6486c4634d0707c
Somnus <nimugates@gmail.com>
2020-04-13 06:23:04 +0200

    overview terminado

--- public/main/game/overviewModule.php
+++ public/main/game/overviewModule.php
@@ -4,5 +4,6 @@
     <p>The game is basically a clicking game. Click the cubes that fall in order to obtain points</p>
     <p>These points can be traded in the <a href="index.php?shop">shop</a> for stuff, including skins and more!</p>
     
+    <img src="assets/images/others/overview.png" width="100%;">
     </div>
 </div>
--- public/main/game/videosModule.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo '
-        <div class="post">
-            <h1>Under Maintenance</h1>
-            <p>Our crew is rendering the highest quality video possible, please be patient, rendering takes ages on a Pentium I</p>
-        </div>';
-?>
